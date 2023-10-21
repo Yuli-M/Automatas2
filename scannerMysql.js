c5329b4beb0c36cf8e20e0f56ff5d3e96b710742
Yuli-M <117542133+Yuli-M@users.noreply.github.com>
2023-10-21 00:20:55 -0700

    primer commit

--- scannerMysql.js
+++ scannerMysql.js
@@ -1,79 +1,49 @@
 /* Yuli Adanesne Moreno Sanchez
-
  * 21760028
  * Lexico para analizar querys de MySQL
  * */
+
 function scanner(input) {
     const palabrasReservadas = [
-        "SELECT", "ABORT", "ABSOLUTE", "ACCESS", "ACTION", "ADD", "AFTER", "ALL", "ALLOCATE", "ALTER", "AND", "ANY", "ARE", "AS", "ASC", "ASSERTION", "AT", "AUTHORIZATION", "AVG", "BEGIN", "BETWEEN", "BIT", "BOTH", "BY", "CALL", "CASCADE", "CASCADED", "CASE", "CAST", "CATALOG", "CHAR", "CHARACTER", "CHECK", "CLOSE", "COLLATE", "COLUMN", "COMMIT", "CONNECT", "CONNECTION", "CONSTRAINT", "CONTINUE", "CONVERT", "CORRESPONDING", "COUNT", "CREATE", "CROSS", "CURRENT", "CURRENT_DATE", "CURRENT_TIME", "CURRENT_TIMESTAMP", "CURSOR", "DATABASE", "DATE", "DAY", "DEALLOCATE", "DECLARE", "DEFAULT", "DEFERRABLE", "DEFERRED", "DELETE", "DESC", "DESCRIBE", "DISTINCT", "DO", "DOMAIN", "DOUBLE", "DROP", "ELSE", "END", "END-EXEC", "ESCAPE", "EXCEPT", "EXEC", "EXECUTE", "EXISTS", "EXTERNAL", "FETCH", "FIRST", "FLOAT", "FOR", "FOREIGN", "FOUND", "FROM", "FULL", "FUNCTION", "GET", "GLOBAL", "GO", "GRANT", "GROUP", "HAVING", "HOUR", "IDENTITY", "IF", "IMMEDIATE", "IN", "INDICATOR", "INDEX", "INNER", "INSERT", "INT", "INTEGER", "WHERE",
+        "SELECT", "ABORT", "ABSOLUTE", "ACCESS", "ACTION", "ADD", "AFTER", "ALL", "ALLOCATE", "ALTER", "AND", "ANY", "ARE", "AS", "ASC", "ASSERTION", "AT", "AUTHORIZATION", "AVG", "BEGIN", "BETWEEN", "BIT", "BOTH", "BY", "CALL", "CASCADE", "CASCADED", "CASE", "CAST", "CATALOG", "CHAR", "CHARACTER", "CHECK", "CLOSE", "COLLATE", "COLUMN", "COMMIT", "CONNECT", "CONNECTION", "CONSTRAINT", "CONTINUE", "CONVERT", "CORRESPONDING", "COUNT", "CREATE", "CROSS", "CURRENT", "CURRENT_DATE", "CURRENT_TIME", "CURRENT_TIMESTAMP", "CURSOR", "DATABASE", "DATE", "DAY", "DEALLOCATE", "DECLARE", "DEFAULT", "DEFERRABLE", "DEFERRED", "DELETE", "DESC", "DESCRIBE", "DISTINCT", "DO", "DOMAIN", "DOUBLE", "DROP", "ELSE", "END", "END-EXEC", "ESCAPE", "EXCEPT", "EXEC", "EXECUTE", "EXISTS", "EXTERNAL", "FETCH", "FIRST", "FLOAT", "FOR", "FOREIGN", "FOUND", "FROM", "FULL", "FUNCTION", "GET", "GLOBAL", "GO", "GRANT", "GROUP", "HAVING", "HOUR", "IDENTITY", "IF", "IMMEDIATE", "IN", "INDICATOR", "INDEX", "INNER", "INSERT", "INT", "INTEGER", "WHERE"
     ];
-
 
     const operadores = [
-  ' ', '+', '-', '*', '/', '%', '&', '|', '^', '=', '>', '<', '>=', '<=', '<>', "'", '"', ';', ',', '`', '(', ')', '[', ']', '{', '}', '+=', '-=', '*=', '/=', '%=', '&=', '^-=', '|*=',
-  'ALL', 'AND', 'ANY', 'BETWEEN', 'EXISTS', 'IN', 'LIKE', 'NOT', 'OR'
+        ' ', '+', '-', '*', '/', '%', '&', '|', '^', '=', '>', '<', '>=', '<=', '<>', "'", '"', ';', ',', '`', '(', ')', '[', ']', '{', '}', '+=', '-=', '*=', '/=', '%=', '&=', '^-=', '|*=',
+        'ALL', 'AND', 'ANY', 'BETWEEN', 'EXISTS', 'IN', 'LIKE', 'NOT', 'OR'
     ];
-
 
     const delimitadores = [
-    ';', '(', ')', ',', '[',']', "'", '"', '{', '}', ':',  '|',  '&',   '<',  '>',   '=',   '.', 
+        ';', '(', ')', ',', '[', ']', "'", '"', '{', '}', ':', '|', '&', '<', '>', '=', '.'
     ];
 
-    const numeros = [
-        '0', '1', '2','3','4','5','6','7','8','9',
-    ];
+    const numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    const isAlphaNumeric = (char) => {
+    const cadenas = ["'Juan'", '"Ejemplo de cadena"'];
+
+    function esAlphaNumeric(char) {
         return (char >= 'a' && char <= 'z') ||
                (char >= 'A' && char <= 'Z') ||
                (char >= '0' && char <= '9') ||
                char === '_';
-    };
+    }
 
-    function esIdentificadorValido(cadena) {
-        // Verificar la longitud máxima de 64 caracteres
-        if (cadena.length > 64) {
-            return false;
-        }
-
-        // Verificar si el identificador no comienza con un número
-        if (esDigito(cadena.charAt(0))) {
-            return false;
-        }
-
-        // Verificar si el identificador contiene solo caracteres válidos
-        if (!contieneCaracteresValidos(cadena)) {
-            return false;
-        }
-
-        if (palabrasReservadas.includes(cadena.toUpperCase())) {
-            return false;
-        }
-
-        // Si pasa todas las validaciones, es un identificador válido
-        return true;
-        }
-
-        function esDigito(caracter) {
-        return '0' <= caracter && caracter <= '9';
-        }
-
-        function contieneCaracteresValidos(cadena) {
+    function noContieneCaracteresInvalidos(cadena) {
         for (let i = 0; i < cadena.length; i++) {
             const caracter = cadena.charAt(i);
-            if (
-            ``!(esLetra(caracter) || esDigito(caracter) || caracter === '_')
-            ) {
+            if (!esAlphaNumeric(caracter)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function esIdentificadorValido(cadena) {
+        if (cadena.length > 64 || esAlphaNumeric(cadena.charAt(0)) || !noContieneCaracteresInvalidos(cadena) || palabrasReservadas.includes(cadena.toUpperCase())) {
             return false;
         }
+        return true;
     }
-    return true;
-}       
-
-        function esLetra(caracter) {
-        return ('a' <= caracter && caracter <= 'z') || ('A' <= caracter && caracter <= 'Z');
-        }
-
 
     let posicionActual = 0;
     const tokens = [];
@@ -82,21 +52,21 @@ function scanner(input) {
         let char = input[posicionActual];
         let token = { type: null, value: '' };
 
-        if (palabrasReservadas[char]) {
+        if (palabrasReservadas.includes(char)) {
             // palabras reservadas
             token.type = 'PALABRA_RESERVADA';
-            token.value = palabrasReservadas[char];
+            token.value = char;
             tokens.push(`${char} : ${token.value}`);
             posicionActual++;
-        } else if (operadores[char]) {
+        } else if (operadores.includes(char)) {
             // operadores
             token.type = 'OPERADOR';
-            token.value = operadores[char];
+            token.value = char;
             tokens.push(`${char} : ${token.value}`);
             posicionActual++;
-        } else if (identificadores[char]) {
+        } else if (esIdentificadorValido(char)) {
             // identificadores
-            while (identificadores[char]) {
+            while (esIdentificadorValido(char)) {
                 token.value += char;
                 posicionActual++;
                 char = input[posicionActual];
@@ -125,18 +95,21 @@ function scanner(input) {
             token.value += char;
             tokens.push(`${token.value} : CADENA`);
             posicionActual++;
-        } else if (delimitadores[char]) {
+        } else if (delimitadores.includes(char)) {
             // delimitadores
             token.type = 'DELIMITADOR';
-            token.value = delimitadores[char];
+            token.value = char;
             tokens.push(`${char} : ${token.value}`);
             posicionActual++;
         } else if (char === ' ') {
             // saltar espacios
             posicionActual++;
-        } // caracteres no reconocidos
+        } else {
+            // caracteres no reconocidos
             tokens.push(`${char} : Caracter no reconocido`);
             posicionActual++;
+        }
+    }
 
     return tokens;
 }
@@ -145,4 +118,5 @@ function scanner(input) {
 const query = "SELECT * FROM usuarios WHERE nombre = 'Juan'";
 const result = scanner(query);
 console.log(result);
+
 
